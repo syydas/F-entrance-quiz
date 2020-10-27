41c5c92509146ff9b6bbe29f947d5e34bbaa74b3
Suqin <suqin.zheng@thoughtworks.com>
2020-10-27 23:10:15 +0800

    [Suqin] feat: Add click button to add students

--- src/components/StudentsList/StudentsList.js
+++ src/components/StudentsList/StudentsList.js
@@ -2,17 +2,34 @@ import React from 'react';
 import './StudentsList.scss';
 
 class StudentList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      display: true,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>学员列表</h1>
-        <ul className="students-list">
+        <div className="students-list">
           {this.props.students.map((student) => (
-            <li key={student.id}>
+            <span key={student.id} className="students-name">
               {student.id}. {student.name}
-            </li>
+            </span>
           ))}
-        </ul>
+          {this.state.display && <input type="name" />}
+          <button type="button" onClick={this.handleClick.bind(this)}>
+            +添加学员
+          </button>
+        </div>
       </div>
     );
   }
